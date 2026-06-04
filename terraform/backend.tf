3f95708c680d43d7b909c127f9fd180b778539f6
Kunja Bhat <kunjabhat0@gmail.com>
2026-06-04 13:19:33 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,1 +1,8 @@
-
+terraform {
+  backend "s3" {
+    bucket         = "kunja-tf-state"
+    key            = "eks/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-lock"
+  }
+}
