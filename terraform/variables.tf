3eb475ec4d369cbf904a28786a0d8a1d88873811
Kunja Bhat <kunjabhat0@gmail.com>
2026-06-04 13:37:20 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,11 @@
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+  default     = "ap-south-1"
+}
 
+variable "environment" {
+  description = "Environment"
+  type        = string
+  default     = "dev"
+}
