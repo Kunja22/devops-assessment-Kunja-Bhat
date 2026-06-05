c3711c2f2652fa5049c8a5db8db5b21bce78431b
Kunja Bhat <kunjabhat0@gmail.com>
2026-06-05 15:07:11 +0530

    Update app.js

--- Application Deployment (Docker + Kubernetes)/app/backend/app.js
+++ Application Deployment (Docker + Kubernetes)/app/backend/app.js
@@ -1,1 +1,32 @@
+const express = require("express");
+const app = express();
 
+app.get("/", (req, res) => {
+  console.log(JSON.stringify({
+    level: "INFO",
+    endpoint: "/",
+    message: "Root API called"
+  }));
+
+  res.json({ message: "Backend Running" });
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "healthy"
+  });
+});
+
+app.get("/metrics", (req, res) => {
+  res.json({
+    uptime: process.uptime(),
+    memory: process.memoryUsage()
+  });
+});
+
+app.listen(3000, () => {
+  console.log(JSON.stringify({
+    level: "INFO",
+    message: "Backend Started"
+  }));
+});
