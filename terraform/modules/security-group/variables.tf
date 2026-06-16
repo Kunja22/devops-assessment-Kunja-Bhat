612f7ddbd505db87c2895647061ae95d150961eb
Kunja Bhat <kunjabhat0@gmail.com>
2026-06-16 15:54:56 +0530

    Delete terraform directory

--- terraform/modules/security-group/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "vpc_id" {
-  type = string
-}
-
-variable "sg_name" {
-  type = string
-}
--- terraform/vpc.tf
+++
@@ -1,7 +0,0 @@
-module "vpc" {
-  source  = "clouddrove/vpc/aws"
-  version = "2.0.2"
-
-  name        = "assessment-vpc"
-  environment = "dev"
-}
